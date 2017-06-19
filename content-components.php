c6395b49f4ef191932b50302ae840dad85599ca4
Kirk Pettinga <kirk.pettinga@gmail.com>
2017-06-19 12:11:40 -0400

    shorten component partial names

--- content-components.php
+++ content-components.php
@@ -1,8 +1,8 @@
 
 <div class="u-my-3">
-    <?php get_template_part( 'partials/component-promo-news' ); ?>
+    <?php get_template_part( 'partials/comp-promo-news' ); ?>
 </div>
 
 <div class="u-my-3">
-    <?php get_template_part( 'partials/component-promo-bio-quote' ); ?>
+    <?php get_template_part( 'partials/comp-promo-bio-quote' ); ?>
 </div>
