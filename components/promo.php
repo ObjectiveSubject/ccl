2bca4774fc907922fd1d700fbdc27c1b135b7720
Kirk Pettinga <kirk.pettinga@gmail.com>
2017-06-26 12:14:42 -0400

    work on grid system

--- components/promo.php
+++ components/promo.php
@@ -1,24 +1,22 @@
-<div class="promo">
+<div class="promo columns">
 
-    <header class="promo__header">
+    <header class="promo__header column is-one-third-tablet is-one-quarter-desktop">
         <div class="promo__title">Default Promo</div>
         <p class="promo__action"><a href="#" class="h4 u-color-black u-color-hover-red">View All</a></p>
     </header>
 
-    <div class="promo__content">        
+    <div class="promo__content column">        
 
-        <div class="grid-row">
+        <div class="columns is-multiline is-mobile">
 
             <?php 
             $i = 0;
             do { ?>
-                <article class="grid-2">
+                <article class="column is-half-mobile is-half-tablet is-one-third-desktop">
                     <a href="#">
-                        <?php if ($i !== 1) : ?>
-                            <div class="u-mb-nudge"><img src="http://unsplash.it/400/<?php echo 400 - ($i * 50); ?>/"/></div>
-                        <?php endif; ?>
+                        <div class="u-mb-nudge"><img src="http://unsplash.it/400/400/"/></div>
                         <p class="h4 u-mt-0">Library Acquires Portait Collection</p>
-                        <p class="h4 u-mt-nudge u-color-muted">March <?php echo $i * 5 + 5; ?></p>
+                        <p class="h4 u-mt-nudge u-color-muted">March 5</p>
                         <p class="u-mt-nudge">Itaquia voluptati optae con reti optae con reris aut ad quiaspe ruptam quibus. Icaepro deseque lacerum cus abore pratque.</p>
                     </a>
                 </article>
