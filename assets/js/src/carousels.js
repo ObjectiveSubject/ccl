1a3bde48dac345017328f0ad12ccf156ac02a671
Kirk Pettinga <kirk.pettinga@gmail.com>
2017-06-29 09:47:26 -0400

    convert carousel JS to constructor

--- assets/js/src/carousels.js
+++ assets/js/src/carousels.js
@@ -9,40 +9,49 @@
 	'use strict';
 	var document = window.document;
 
-    var carousels = {
+    var Carousel = function(el){
 
-        init: function() {
-            
-            $('.js-promo-carousel').each(function(){
-                carousels.promo(this);
-            });
+        this.$el = $(el);
+        this.globalDefaults = {
+            mobileFirst: true,
+            dotsClass: 'ccl-c-carousel__paging',
+        };
 
-        },
+        this.init();
 
-        promo: function(el) {
-            var $this = $(el),
-                defaults = {
-                    infinite: false,
-                    mobileFirst: true,
-                    dots: true,
-                    dotsClass: 'ccl-c-carousel__paging slick-dots',
-                    slidesToScroll: 1
-                },
-                options = $this.data('slick');
+    };
 
-            options = $.extend( defaults, options );
-
-            var carousel = $this.slick(options);
-            carousel.on('beforeChange', function(event, slick, currentSlide, nextSlide){
-                $this.find('.slick-slide').removeClass('ccl-is-past');
-                $this.find('.slick-slide[data-slick-index="'+nextSlide+'"]').prevAll().addClass('ccl-is-past');
-            });
+    Carousel.prototype.init = function() {
+        
+        if ( this.$el.hasClass('js-promo-carousel') ) {
+            this.promo();
         }
 
     };
 
+    Carousel.prototype.promo = function() {
+        var defaults = {
+                infinite: false,
+                dots: true,
+                slidesToScroll: 1
+            },
+            options = this.$el.data('slick');
+
+        options = $.extend( this.globalDefaults, defaults, options );
+
+        var carousel = this.$el.slick(options),
+            _this = this;
+
+        carousel.on('beforeChange', function(event, slick, currentSlide, nextSlide){
+            _this.$el.find('.slick-slide').removeClass('ccl-is-past');
+            _this.$el.find('.slick-slide[data-slick-index="'+nextSlide+'"]').prevAll().addClass('ccl-is-past');
+        });
+    };
+
     $(document).ready(function(){
-        carousels.init();
+        $('.ccl-c-carousel').each(function(){
+            new Carousel(this);
+        });
     });
 
 } )( this, jQuery );
