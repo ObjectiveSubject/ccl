4f1a1b4690e5251ab2e7b73c4866a1ccf3c6de64
Kirk Pettinga <kirk.pettinga@gmail.com>
2017-07-07 12:43:19 -0400

    build out basic page template

--- page.php
+++ page.php
@@ -7,14 +7,39 @@ get_header(); ?>
 
 	<div class="site-content">
 
-		<?php while ( have_posts() ) : the_post(); ?>
+		<?php while ( have_posts() ) : the_post();
+		
+			$thumb_url = get_the_post_thumbnail_url( $post, 'full' ); ?>
 
 			<article <?php post_class(); ?>>
 
-				<h1 class="entry-title"><?php the_title(); ?></h1>
+				<div class="ccl-c-hero" style="background-image:url(<?php echo esc_url( $thumb_url ); ?>)">
+					<div class="ccl-c-hero__container">
+						
+						<div class="ccl-c-hero__header">
+							<h1 class="ccl-c-hero__title"><?php the_title(); ?></h1>
+						</div>
+					
+						<div class="ccl-c-hero__content">
+							
+							<div class="ccl-h4 ccl-u-mt-0"><?php the_excerpt(); ?></div>
 
-				<div class="entry-content">
-					<?php the_content(); ?>
+						</div>
+					
+					</div>
+
+				</div>
+
+				<div class="ccl-l-container">
+					
+					<div class="ccl-l-row">
+
+						<div class="ccl-l-column ccl-l-span-8-md ccl-u-mt-2">
+							<?php the_content(); ?>
+						</div>
+
+					</div>
+					
 				</div>
 
 			</article>
