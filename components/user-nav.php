c6b146db8e3eaf95bb1bf1cc117ae1d839c842ad
Kirk Pettinga <kirk.pettinga@gmail.com>
2017-07-13 14:22:53 -0500

    add user-nav component

---
+++ components/user-nav.php
@@ -0,0 +1,26 @@
+<nav class="ccl-c-user-nav">
+
+    <div class="ccl-l-container">
+        
+        <ul class="ccl-c-user-nav__menu">
+
+            <li class="ccl-h5 ccl-u-mt-0 ccl-u-py-nudge"><span class="ccl-u-faded">Welcome</span> User_Name</li>
+            
+            <li class="ccl-c-user-nav__menu-item ccl-c-dropdown">
+                <button href="#dropdown-1" class="ccl-c-dropdown__toggle"><span class="ccl-u-faded">Welcome</span> <User_Name></User_Name></button>
+                <ul id="dropdown-1" class="ccl-c-dropdown__menu">
+                    <li class="ccl-c-dropdown__item"><a href="#">Dropdown Item</a></li>
+                    <li class="ccl-c-dropdown__item"><a href="#">Dropdown Item</a></li>
+                    <li class="ccl-c-dropdown__item"><a href="#">Dropdown Item</a></li>
+                    <li class="ccl-c-dropdown__item"><a href="#">Dropdown Item</a></li>
+                    <li class="ccl-c-dropdown__item"><a href="#">Dropdown Item</a></li>
+                </ul>
+            </li>
+
+            <li class="ccl-h5 ccl-u-mt-0 ccl-u-py-nudge"><span class="ccl-u-faded">Chat with a librarian</span></li>
+
+        </ul>
+
+    </div>
+
+</nav>
