5dbabc52cc021d7d107530e9167ba177f9b9b70a
Kirk Pettinga <kirk.pettinga@gmail.com>
2017-07-14 13:34:51 -0500

    show two styles of dropdown

--- components/dropdown.php
+++ components/dropdown.php
@@ -1,10 +1,33 @@
-<div class="ccl-c-dropdown">
-    <button href="#dropdown-1" class="ccl-c-dropdown__toggle ccl-b-btn">Dropdown Toggle <i class="ccl-b-caret-down" aria-hidden="true"></i></button>
-    <ul id="dropdown-1" class="ccl-c-dropdown__menu">
-        <li class="ccl-c-dropdown__item"><a href="#">Dropdown Item</a></li>
-        <li class="ccl-c-dropdown__item"><a href="#">Dropdown Item</a></li>
-        <li class="ccl-c-dropdown__item"><a href="#">Dropdown Item</a></li>
-        <li class="ccl-c-dropdown__item"><a href="#">Dropdown Item</a></li>
-        <li class="ccl-c-dropdown__item"><a href="#">Dropdown Item</a></li>
-    </ul>
-</div>
+<!--<div class="ccl-l-row">
+
+    <div class="ccl-l-column">-->
+    
+        <div class="ccl-c-dropdown">
+            <a href="#dropdown-1" class="ccl-c-dropdown__toggle">Default Toggle <i class="ccl-b-caret-down" aria-hidden="true"></i></a>
+            <ul id="dropdown-1" class="ccl-c-dropdown__menu">
+                <li class="ccl-c-dropdown__item"><a href="#">Dropdown Item</a></li>
+                <li class="ccl-c-dropdown__item"><a href="#">Dropdown Item</a></li>
+                <li class="ccl-c-dropdown__item"><a href="#">Dropdown Item</a></li>
+                <li class="ccl-c-dropdown__item"><a href="#">Dropdown Item</a></li>
+                <li class="ccl-c-dropdown__item"><a href="#">Dropdown Item</a></li>
+            </ul>
+        </div>
+    
+    <!--</div>
+    
+    <div class="ccl-l-column">-->
+    
+        <div class="ccl-c-dropdown ccl-u-mt-1">
+            <button class="ccl-c-dropdown__toggle ccl-b-btn">Button Toggle <i class="ccl-b-caret-down" aria-hidden="true"></i></button>
+            <ul id="dropdown-1" class="ccl-c-dropdown__menu">
+                <li class="ccl-c-dropdown__item"><a href="#">Dropdown Item</a></li>
+                <li class="ccl-c-dropdown__item"><a href="#">Dropdown Item</a></li>
+                <li class="ccl-c-dropdown__item"><a href="#">Dropdown Item</a></li>
+                <li class="ccl-c-dropdown__item"><a href="#">Dropdown Item</a></li>
+                <li class="ccl-c-dropdown__item"><a href="#">Dropdown Item</a></li>
+            </ul>
+        </div>
+    
+    <!--</div>
+
+</div>-->
