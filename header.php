2f7bdaa1907f780386fa0c5c342580c32aef7f20
Kirk Pettinga <kirk.pettinga@gmail.com>
2017-07-14 21:56:18 -0500

    header markup

--- header.php
+++ header.php
@@ -16,10 +16,60 @@
 
 		<?php get_template_part( 'components/user-nav' ); ?>
 
-		<header id="masthead" class="ccl-c-masthead " role="banner">
+		<header id="masthead" class="ccl-c-masthead ccl-u-mb-1" role="banner">
 			
 			<div class="ccl-l-container">
-				<h2 class="ccl-u-pb-1 ccl-u-faded">(CCL Header)</h2>
+				
+				<div class="site-title ccl-h2" style="font-family: Georgia,serif">
+					<a href="#">Claremont Colleges Library • VIT@L</a>
+				</div>
+
+				<hr/>
+
+				<div class="ccl-l-row">
+					<div class="ccl-l-column">
+						<nav>
+							<ul class="ccl-u-clean-list ccl-u-mt-1">
+								<li class="ccl-h3 ccl-u-mt-nudge"><a href="#">About Us</a></li>
+								<li class="ccl-h3 ccl-u-mt-nudge"><a href="#">Using the Library</a></li>
+								<li class="ccl-h3 ccl-u-mt-nudge"><a href="#">Learning Spaces</a></li>
+								<li class="ccl-h3 ccl-u-mt-nudge"><a href="#">Collections</a></li>
+								<li class="ccl-h3 ccl-u-mt-nudge"><a href="#">Research Support</a></li>
+							</ul>
+						</nav>	
+					</div>
+					<div class="ccl-l-column">
+						<nav>
+							<ul class="ccl-u-clean-list ccl-u-mt-1">
+								<li class="ccl-h4 ccl-u-mt-nudge"><a href="#">Faculty Resources</a></li>
+								<li class="ccl-h4 ccl-u-mt-nudge"><a href="#">News & Events</a></li>
+								<li class="ccl-h4 ccl-u-mt-nudge"><a href="#">Chat with a Librarian</a></li>
+							</ul>
+						</nav>
+					</div>
+					<div class="ccl-l-column">
+						<ul class="ccl-u-clean-list ccl-u-mt-1">
+							<li>
+								<hr class="ccl-u-my-nudge"/>
+								<span class="ccl-h5"><i class="ccl-b-icon-clock" aria-hidden="true"></i> Today’s hours 7:30am - 9pm</span>
+							</li>
+							<li>
+								<hr class="ccl-u-my-nudge"/>
+								<span class="ccl-h5"><i class="ccl-b-icon-calendar" aria-hidden="true"></i> Relevent Calendar Item</span>
+							</li>
+							<li>
+								<hr class="ccl-u-my-nudge"/>
+								<span class="ccl-h5">
+									<span style="color: red">
+										<i class="ccl-b-icon-alert" aria-hidden="true"></i> Notice:
+									</span>
+									Relevent notice message
+								</span>
+							</li>
+						</ul>
+					</div>
+				</div>
+
 			</div>
 
 		</header><!-- #masthead -->
