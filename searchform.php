db20e9904edd2445e5147f14bcf3a4e6cf87a400
Dave Kellam <dave@eightface.com>
2017-07-24 23:40:25 -0400

    Default base WordPress search form

---
+++ searchform.php
@@ -0,0 +1,7 @@
+<form id="header-search-form" role="search" method="get" class="header-search-form search-form" action="<?php echo site_url(); ?>">
+	<label for="s" class="u-hug u-display-none">
+		<?php _e( 'What are you looking for?', 'ccl' ); ?>
+	</label>
+	<input id="s" type="search" class="search-field h3 u-hug" placeholder="<?php _e( 'Search', 'ccl' ); ?>" value="" name="s" title="Search for:">
+	<input type="submit" class="search-submit" value="<?php _e( 'Press Enter', 'ccl' ); ?>">
+</form>
