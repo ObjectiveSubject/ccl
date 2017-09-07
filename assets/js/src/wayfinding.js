e7d4e22945a3e33ab8e0453ed4490958f1fd0be7
Kirk Pettinga <kirk.pettinga@gmail.com>
2017-09-07 16:09:04 +0200

    implement wayfinding logic refs #11

--- assets/js/src/wayfinding.js
+++ assets/js/src/wayfinding.js
@@ -6,22 +6,103 @@
 
 ( function( window, $ ) {
 	'use strict';
-	var document = window.document;
+    var document = window.document;
+    
+    var Tabs = function(el) {
+
+        var _this = this;
+
+        this.$el = $(el);
+        this.$tabs = this.$el.find('.ccl-c-tab');
+        this.$tabContents = $('.ccl-c-tab__content');
+        
+
+        this.$tabs.each(function(){
+            var $tab = $(this);
+            $tab.click(function(e){
+                e.preventDefault();
+                var target = $tab.data('target');
+                console.log(target, _this.$tabContents);
+                _this.$tabContents.removeClass('ccl-is-active');
+                _this.$tabContents.filter(target).addClass('ccl-is-active');
+            });
+        });
+
+        console.log(this);
+    };
 
     var Wayfinder = function(el){
         this.$el = $(el);
+        this.$input = this.$el.find('#call-num-search');
+        this.$marquee = this.$el.find('.ccl-c-wayfinder__marquee');
+        this.$callNum = this.$el.find('.ccl-c-wayfinder__call-num');
+        this.$level = this.$el.find('.ccl-c-wayfinder__level');
         this.init();
     };
 
     Wayfinder.prototype.init = function() {
         
-        
+        var _this = this,
+            timeout;
+
+        this.$input
+            .keyup(function () {
+                var query = $(this).val();
+                
+                clearTimeout(timeout);
+                
+                _this.$callNum.text(query);
+
+                if ( query === "" ) {
+                    _this.$marquee.hide();                    
+                } else {
+                    _this.$marquee.show();
+                }
+
+                timeout = setTimeout(function () {
+                    if ( query !== "" ) {
+
+                        _this.findRoom( query );
+
+                    } else {
+
+                        _this.$marquee.hide();
+                        _this.$callNum.text('');
+                        _this.$level.text('');
+
+                    }
+                }, 600);
+            })
+            .keydown(function (event) {
+
+                clearTimeout(timeout);
+
+                _this.$level.text('...');
+                _this.reset();
+            });
 
     };
 
+    Wayfinder.prototype.findRoom = function(query) {
+        this.$el.addClass('ccl-app-active');
+        this.$level.text('Level 1');
+        this.$el.find('a[href="#floor-1"]').addClass('ccl-u-color-blue');
+        this.$el.find('#floor-1__room-1').addClass('ccl-is-active');
+        $('html, body').animate({ scrollTop: $('.ccl-c-search').offset().top });
+    };
+
+    Wayfinder.prototype.reset = function() {
+        this.$el.removeClass('ccl-app-active');
+        this.$el.find('a[href*="#floor-"]').removeClass('ccl-u-color-blue');
+        this.$el.find('.ccl-c-floor__room').removeClass('ccl-is-active');
+    };
+
     $(document).ready(function(){
+        $('.ccl-js-tabs').each(function(){
+            new Tabs(this);
+        });
         $('.ccl-js-wayfinder').each(function(){
-            new Carousel(this);
+            new Wayfinder(this);
         });
     });
 
--- page-templates/demo-wayfinding.php
+++ page-templates/demo-wayfinding.php
@@ -35,13 +35,16 @@ get_header(); ?>
 
 					<div class="ccl-l-container">
 
-						<form class="ccl-c-search__bar">
-							<input type="text" class="ccl-b-input" placeholder="Enter a call number to begin"/>
-							<button type="submit" class="ccl-c-search__submit">
-								<i class="ccl-b-icon-search" aria-hidden="true"></i>
-								<span class="ccl-u-display-none">Search</span>
-							</button>
-						</form>
+						<div class="ccl-l-row">
+							<div class="ccl-l-column">
+								<form class="ccl-c-search__bar">
+									<input id="call-num-search" name="call-num-search" type="text" class="ccl-b-input" placeholder="Enter a call number to begin"/>
+								</form>
+							</div>
+							<div class="ccl-l-column">
+								<h3 class="ccl-c-wayfinder__marquee">Item with call number "<span class="ccl-c-wayfinder__call-num">JC 177 B3 1895</span>"<br/>can be found on <span class="ccl-c-wayfinder__level">...</span></h3>
+							</div>
+						</div>
 					
 					</div>
 
@@ -49,29 +52,27 @@ get_header(); ?>
 
 				<div class="ccl-l-container">
 
-					<h3>Item with call number "<span class="ccl-u-color-blue">JC 177 B3 1895</span>" can be found in "<span class="ccl-u-color-blue">Room Name</span>"</h3>
-
 					<div class="ccl-l-row ccl-u-mt-2">
 						<div class="ccl-l-column--lazy-lg ccl-l-span-full" style="min-width: 120px; border-right:1px solid #ddd">
-							<ul class="ccl-u-clean-list ccl-u-mt-1">
-								<li class="ccl-u-mb-1 ccl-u-pb-1" style="border-bottom:1px solid #ddd"><a href="#floor-1" class="ccl-h4 ccl-u-color-blue">Level 1</a></li>
-								<li class="ccl-u-mb-1 ccl-u-pb-1" style="border-bottom:1px solid #ddd"><a href="#floor-1" class="ccl-h4">Level 2</a></li>
-								<li class="ccl-u-mb-1 ccl-u-pb-1" style="border-bottom:1px solid #ddd"><a href="#floor-1" class="ccl-h4">Level 3</a></li>
-								<li class="ccl-u-mb-1 ccl-u-pb-1" style="border-bottom:1px solid #ddd"><a href="#floor-1" class="ccl-h4">Level 4</a></li>
+							<ul class="ccl-u-clean-list ccl-js-tabs ccl-u-mt-1">
+								<li class="ccl-u-mb-1 ccl-u-pb-1" style="border-bottom:1px solid #ddd"><a href="#floor-1" class="ccl-c-tab ccl-h4" data-target="#floor-1">Level 1</a></li>
+								<li class="ccl-u-mb-1 ccl-u-pb-1" style="border-bottom:1px solid #ddd"><a href="#floor-2" class="ccl-c-tab ccl-h4" data-target="#floor-2">Level 2</a></li>
+								<li class="ccl-u-mb-1 ccl-u-pb-1" style="border-bottom:1px solid #ddd"><a href="#floor-3" class="ccl-c-tab ccl-h4" data-target="#floor-3">Level 3</a></li>
+								<li class="ccl-u-mb-1 ccl-u-pb-1" style="border-bottom:1px solid #ddd"><a href="#floor-4" class="ccl-c-tab ccl-h4" data-target="#floor-4">Level 4</a></li>
 							</ul>	
 						</div>
-						<div class="ccl-l-column">
-							<div id="floor-1" class="">
+						<div class="ccl-c-wayfinder__maps ccl-l-column">
+							<div id="floor-1" class="ccl-c-tab__content ccl-is-active">
 								<div><?php include( CCL_PATH . '/assets/images/level-1-flat.svg' ) ?></div>
 							</div>
-							<div id="floor-2" class="">
-								<div>floor image here...<?php //include( CCL_PATH . '/assets/images/level-2.svg' ) ?></div>
+							<div id="floor-2" class="ccl-c-tab__content">
+								<div><?php include( CCL_PATH . '/assets/images/level-2-flat.svg' ) ?></div>
 							</div>
-							<div id="floor-3" class="">
-								<div>floor image here...<?php //include( CCL_PATH . '/assets/images/level-3.svg' ) ?></div>
+							<div id="floor-3" class="ccl-c-tab__content">
+								<div><?php include( CCL_PATH . '/assets/images/level-3-flat.svg' ) ?></div>
 							</div>
-							<div id="floor-4" class="">
-								<div>floor image here...<?php //include( CCL_PATH . '/assets/images/level-4.svg' ) ?></div>
+							<div id="floor-4" class="ccl-c-tab__content">
+								<div><?php include( CCL_PATH . '/assets/images/level-4-flat.svg' ) ?></div>
 							</div>	
 						</div>
 					</div>
