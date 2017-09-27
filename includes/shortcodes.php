e79ba9f02f70173033c8f04518f65819ae4608e0
Kirk Pettinga <kirk.pettinga@gmail.com>
2017-09-27 15:12:56 +0200

    add accordion shortcode #21

--- includes/shortcodes.php
+++ includes/shortcodes.php
@@ -11,28 +11,61 @@ function setup() {
 		return __NAMESPACE__ . "\\$function";
 	};
 
-	add_shortcode( 'scheduler', $n( 'schedule_appointment' ) );
+	add_shortcode( 'scheduler', $n( 'scheduler_fn' ) );
+	add_shortcode( 'accordion', $n( 'accordion_fn' ) );
 }
 
 
- /**
-  * Create a scheduler shortcode to display the Libcal MyScheduler widget for booking appointments
-  *
-  * @param $attributes array List of attributes from the given shortcode
-  *
-  * @return mixed HTML output for the shortcode
-  */
- function schedule_appointment( $attributes ) {
+/**
+* Create a scheduler shortcode to display the Libcal MyScheduler widget for booking appointments
+*
+* @param $attributes array List of attributes from the given shortcode
+*
+* @return mixed HTML output for the shortcode
+*/
+function scheduler_fn( $attributes ) {
 
- 	$data = shortcode_atts( array(
- 		'text' => "Schedule Appointment",
- 		'height' => 450,
- 		'width'	=> 500,
- 	), $attributes );
+$data = shortcode_atts( array(
+	'text' => "Schedule Appointment",
+	'height' => 450,
+	'width'	=> 500,
+), $attributes );
 
- 	$html = \CCL\Integrations\LibCal\schedule_widget( false, $data );
+$html = \CCL\Integrations\LibCal\schedule_widget( false, $data );
 
- 	// $html = "<b>FARTSTICKS</b>";
+// $html = "<b>FARTSTICKS</b>";
 
- 	return $html;
- }
+return $html;
+}
+
+
+/**
+* Create an accordion
+*
+* @param $attributes array List of attributes from the given shortcode
+*
+* @return mixed HTML output for the shortcode
+*/
+function accordion_fn( $attributes = false, $content = null ) {
+
+	$data = shortcode_atts( array(
+		'title' => "Title",
+	), $attributes );
+
+	$content = preg_replace( '#^<\/p>|^<br \/>|<p>$#', '', $content );
+	ob_start();
+	?>
+	<div class="ccl-c-accordion">
+		<div class="ccl-c-accordion__toggle">
+			<?php echo $data['title']; ?>
+		</div>
+		<div class="ccl-c-accordion__content">
+			<?php echo do_shortcode( shortcode_unautop( trim( $content ) ) ); ?>
+		</div>
+	</div>
+	<?
+	$html = ob_get_contents();
+	ob_get_clean();
+
+	return $html;
+}
