1807bcc18ebf35b5972b05829b2ea2958f2cb3d2
Kirk Pettinga <kirk.pettinga@gmail.com>
2017-10-11 14:49:33 +0200

    fix up 404 template #16

--- 404.php
+++ 404.php
@@ -1,27 +1,50 @@
 <?php
 /**
- * General page template
+ * Error Template
  */
 
-get_header();
-?>
+get_header(); ?>
 
-	<div class="site-content error-404">
+	<div class="site-content">
 
-		<article class="hentry">
+		
 
-			<h1 class="entry-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'ccl' ); ?></h1>
+			<article>
 
-			<div class="entry-content">
+				<div class="ccl-c-hero">
+					<div class="ccl-c-hero__container">
 
-				<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try a search?', 'ccl' ); ?></p>
+						<div class="ccl-c-hero__header">
+							<h1 class="ccl-c-hero__title"><?php _e( 'Oops, nothing was found.', 'ccl' ) ; ?></h1>
+						</div>
 
-				<?php get_search_form(); ?>
+					</div>
 
-			</div><!-- .entry-content -->
+				</div>
 
-		</article>
+				<div class="ccl-l-container">
 
-	</div><!-- .error-404 -->
+					<div class="ccl-l-row">
+						<div class="ccl-c-entry-content ccl-l-column ccl-l-span-8-md ccl-u-my-2">
+
+							<form class="ccl-c-search__bar" role="search" method="get" action="<?php echo site_url(); ?>" style="border:1px solid">
+
+								<input type="text" class="ccl-c-search__bar-item ccl-b-input" name="s" placeholder="Maybe try a search?"/>
+											
+								<button type="submit" class="ccl-c-search__submit">
+									<i class="ccl-b-icon-search" aria-hidden="true"></i>
+									<span class="ccl-u-display-none">Search</span>
+								</button>
+
+							</form>
+
+						</div>
+					</div>
+
+				</div>
+
+			</article>
+
+	</div>
 
 <?php get_footer();
