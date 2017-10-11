ffdd56d376ed4648406121829799ee63cf860f2b
Kirk Pettinga <kirk.pettinga@gmail.com>
2017-10-11 13:17:36 +0200

    add sticky element JS #23

---
+++ assets/js/src/stickies.js
@@ -0,0 +1,63 @@
+/**
+ * Stickies
+ * 
+ * Behaviour for sticky elements.
+ */
+
+( function( window, $ ) {
+	'use strict';
+    var document = window.document,
+        $window = $(window),
+        className = {
+            isFixed: 'ccl-is-fixed'
+        };
+
+    var Sticky = function(el){
+
+        // variables
+        var $el = $(el),
+            height = $el.outerHeight(),
+            offset = $el.offset(),
+            options = $el.data('sticky'),
+            wrapper = $('<div class="js-sticky-wrapper"></div>').css({ height: height + 'px' });
+
+        var defaultOptions = {
+            offset: 0
+        };
+
+        options = $.extend( defaultOptions, options );
+
+        // wrap element
+        $el.wrap( wrapper );
+
+        // scroll listener
+        $window.scroll( CCL.throttle( _onScroll, 100 ) );
+
+        // on scroll
+        function _onScroll() {
+            
+            var scrollTop = $window.scrollTop(),
+                adjustedScrollTop = scrollTop + options.offset;
+    
+            if ( adjustedScrollTop >= offset.top ) {
+                if ( options.offset ) {
+                    $el.css( {top: options.offset + 'px'} );
+                }
+                $el.addClass( className.isFixed );
+            } else {
+                $el.removeClass( className.isFixed );
+            }
+    
+        }
+
+        return this;
+
+    };
+
+    $(document).ready(function(){
+        $('.js-is-sticky').each(function(){
+            new Sticky(this);
+        });
+    });
+
+} )( this, jQuery );
--- components/user-nav.php
+++ components/user-nav.php
@@ -1,4 +1,4 @@
-<nav class="ccl-c-user-nav">
+<nav class="ccl-c-user-nav js-is-sticky" data-sticky='{ "offset": <?php echo is_admin_bar_showing() ? '32' : '0'; ?> }'>
 
     <div class="ccl-l-container">
         
