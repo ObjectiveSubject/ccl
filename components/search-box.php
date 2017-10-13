b3041fd2ce6b62f1def58b958c899bf7a0cbc3be
Dave Kellam <dave@eightface.com>
2017-10-13 11:52:59 -0400

    Add index codes to search options
    Also added titles to the selects for wcag
    
    #18

--- components/search-box.php
+++ components/search-box.php
@@ -9,18 +9,17 @@
 
                 <div class="ccl-c-search-bar__option">
                     <strong class="ccl-u-faded">As:</strong>
-                    <select class="ccl-b-select" name="index">
-                        <option value="all">All</option>
-                        <option value="title">Title</option>
-                        <option value="keyword" selected="selected">Keyword</option>
-                        <option value="author">Author</option>
-                        <option value="subject">Subject</option>
+                    <select class="ccl-b-select" name="index" title="Index">
+                        <option value="ti">Title</option>
+                        <option value="kw" selected="selected">Keyword</option>
+                        <option value="au">Author</option>
+                        <option value="su">Subject</option>
                     </select>
                 </div>
                 
                 <div class="ccl-c-search-bar__option">
                     <strong class="ccl-u-faded">In:</strong>
-                    <select class="ccl-b-select" name="database">
+                    <select class="ccl-b-select" name="database" title="Database">
                         <option value="all" selected="selected">All</option>
                         <option value="Z-wcorg">WorldCat</option>
                         <option value="Z-oais">OAlster</option>
