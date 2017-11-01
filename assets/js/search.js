eb87e88e67c1e07c64892f38b001eef5ac3da581
Kirk Pettinga <kirk.pettinga@gmail.com>
2017-11-01 14:08:23 +0100

    search bar visual tweaks #39

--- assets/js/search.js
+++ assets/js/search.js
@@ -9,53 +9,79 @@
 
 ( function( window, $ ) {
  	'use strict';
-	var document = window.document;
-
+	var document = window.document,
+		TAB = 9, SHIFT = 16, CTRL = 17, ALT = 18, CAPS = 20, ESC = 27, LCMD = 91, RCMD = 92, LARR = 37, UARR = 38, RARR = 39, DARR = 40,
+		forbiddenKeys = [TAB, SHIFT, CTRL, ALT, CAPS, ESC, LCMD, RCMD, LARR, UARR, RARR, DARR];
+		
      var SearchAutocomplete = function(elem){
-		this.input = $(elem);
+		
+		this.$el = $(elem);
+		this.$input = $(elem).find('#ccl-search');
+		this.$responseArea = this.$el.find('.ccl-c-search-results__list'),
+		this.$responseItems = this.$el.find('.ccl-c-search-item'),
+		this.$resultsLink = this.$el.find('.ccl-c-search-results__footer');
+
         this.init();
      };
 
-     SearchAutocomplete.prototype.init = function (elem) {
+     SearchAutocomplete.prototype.init = function () {
 		var _this = this,
 			timeout;
 
-		 this.input
-			 .keyup(function () {
-				 var query = $(this);
-				 clearTimeout(timeout);
-				 timeout = setTimeout(function () {
-					 if (query.val() !== "") {
+		_this.$input
+			.keyup(function (event) {
 
+				if ( forbiddenKeys.indexOf( event.keyCode ) > -1 ) {
+					return;
+				}
+
+				console.log('keyup');
+
+				var query = _this.$input.val();
+
+				if ( query !== '' && (event.which === 13 || event.keyCode === 13) ) {
+					_this.wrapQuery();
+					return;
+				}
+
+				timeout = setTimeout(function () {
+
+					if ( query !== "" && query.length > 1 ) {
+						console.log('fetchResults');
 					 	_this.fetchResults( query );
+					}
+					else {
+						_this.$responseArea.html('');
+					}
 
-					 }
-				 }, 600);
-			 })
-			 .keydown(function (event) {
-			 	 if (event.which === 13 || event.keyCode === 13) {
-					 _this.wrapQuery();
-				 } else {
-					 clearTimeout(timeout);
-				 }
-			 });
+				}, 600);
 
-		 $('.ccl-c-search-bar').submit(function(event) {
-			 _this.wrapQuery();
-		 });
-	 };
+			})
+			.keydown(function (event) {
+
+				if ( forbiddenKeys.indexOf( event.keyCode ) > -1 ) {
+					return;
+				}
+
+				console.log('keydown');
+				 
+				clearTimeout(timeout);
+
+			});
+
+		$('.ccl-c-search-bar').submit(function() {
+			_this.wrapQuery();
+		});
+	};
 
 	SearchAutocomplete.prototype.fetchResults = function( query ) {
 
-		var responseArea = $('.ccl-c-search-results__list'),
-			responseItems = $('.ccl-c-search-item'),
-			resultsLink = $(".ccl-c-search-results__footer");
-
-		var data = {
-			action: 'load_search_results', // this should probably be able to do people & assets too (maybe DBs)
-			query : query.val()
-		};
-
+		var _this = this,
+			data = {
+				action: 'load_search_results', // this should probably be able to do people & assets too (maybe DBs)
+				query : query
+			};
+		
 		$.post(searchAjax.ajaxurl, data, function (response) {
 			var results = $.parseJSON(response),
 				count = results.count,
@@ -63,49 +89,72 @@
 				posts = results.posts;
 
 			// Clear response area list items (update when Pattern Library view isn't necessary)
-			responseItems.remove();
-			resultsLink.remove();
+			_this.$responseArea.html('');
+			_this.$resultsLink.remove();
 
-			var listItem = '<li class="ccl-c-search-item">' +
-				'<a href="https://ccl.on.worldcat.org/external-search?sortKey=library&queryString=' + query + '">' +
-				'<span class="ccl-c-search-item__type">' +
-				'<i class="ccl-b-icon-book" aria-hidden="true"></i>' +
-				'<span class="ccl-c-search-item__type-text">WorldCat</span>' +
-				'</span>' +
-				'<span class="ccl-c-search-item__title\">Search for &ldquo;' + query + '&rdquo; on WorldCat</span>' +
-				'</a>' +
-				'</li>';
+			// Create list item for Worldcat search.
+			var listItem =  '<a href="https://ccl.on.worldcat.org/external-search?sortKey=library&queryString=' + query + '" class="ccl-c-search-item ccl-is-large" role="listitem" target="_blank">' +
+								'<span class="ccl-c-search-item__type">' +
+									'<i class="ccl-b-icon-book" aria-hidden="true"></i>' +
+									'<span class="ccl-c-search-item__type-text">WorldCat</span>' +
+								'</span>' +
+								'<span class="ccl-c-search-item__title\">' +
+									'Search for &ldquo;' + query + '&rdquo; on WorldCat' +
+								'</span>' +
+								'<span class="ccl-c-search-item__cta">' +
+									'<span class="ccl-u-color-school">Search <i class="ccl-b-icon-arrow-right" aria-hidden="true" style="vertical-align:middle"></i></span>' +
+								'</span>' +
+							'</a>';
 
-			responseArea.append(listItem);
+			_this.$responseArea.append(listItem);
 
+			// Create list items for each post in results
 			if ( count > 0 ) {
 				// Build results list
 				posts.forEach(function (post) {
 					console.log(post);
 
-					listItem = '<li class="ccl-c-search-item">' +
-						'<a href="' + post["link"] + '">' +
-						'<span class=\"ccl-c-search-item__type\">' +
-						'<i class="ccl-b-icon-' + post["icon"] + '" aria-hidden="true"></i>' +
-						'<span class="ccl-c-search-item__type-text">' + post["type"] + '</span>' +
-						'</span>' +
-						'<span class="ccl-c-search-item__title">' + post["title"] + '</span>' +
-						'</a>' +
-						'</li>';
+					var cta,
+						target;
 
-					responseArea.append(listItem);
+					switch( post["type"] ) {
+						case 'Database':
+							cta = 'View on Worldcat';
+							target = '_blank';
+							break;
+						case 'Librarian':
+							cta = 'Contact';
+							target = '_blank';
+							break;
+						default:
+							cta = 'View';
+							target = '_self';
+					}
+
+					listItem =  '<a href="' + post["link"] + '" class="ccl-c-search-item" role="listitem" target="' + target + '">' +
+									'<span class=\"ccl-c-search-item__type\">' +
+										'<i class="ccl-b-icon-' + post["icon"] + '" aria-hidden="true"></i>' +
+										'<span class="ccl-c-search-item__type-text">' + post["type"] + '</span>' +
+									'</span>' +
+									'<span class="ccl-c-search-item__title">' + post["title"] + '</span>' +
+									'<span class="ccl-c-search-item__cta">' +
+										'<span>' + cta + ' <i class="ccl-b-icon-arrow-right" aria-hidden="true" style="vertical-align:middle"></i></span>' +
+									'</span>' +
+								'</a>';
+
+					_this.$responseArea.append(listItem);
 				});
 			}
 
 			// Build results count/link
-			listItem = '<li class="ccl-c-search-results__footer">' +
-				       '<a href="/?s=' + query + '" class="ccl-c-search-results__action">' +
-				       'View all ' + count + ' Results ' +
-				       '<i class="ccl-b-icon-arrow-right" aria-hidden="true"></i>' +
-				       '</a>' +
-			           '</li>';
+			listItem = '<div class="ccl-c-search-results__footer">' +
+							'<a href="/?s=' + query + '" class="ccl-c-search-results__action">' +
+								'View all ' + count + ' Results ' +
+								'<i class="ccl-b-icon-arrow-right" aria-hidden="true"></i>' +
+							'</a>' +
+			           '</div>';
 
-			responseArea.append(listItem);
+			_this.$responseArea.append(listItem);
 
 		});
 
@@ -127,7 +176,7 @@
 
      $(document).ready(function(){
 		// .each() will fail gracefully if no elements are found
-		$('#ccl-search').each(function(){
+		$('.ccl-c-search').each(function(){
 			new SearchAutocomplete(this);
 		 });
      });
--- components/search-box.php
+++ components/search-box.php
@@ -1,46 +1,38 @@
-<div class="ccl-u-py-6" style="background-color: #eee;">
+<div class="ccl-c-search">
 
-    <div class="ccl-l-container">
+    <form class="ccl-c-search-bar" name="catalogSearch" action="http://ccl.on.worldcat.org/search" target="_blank">
+        <input type="text" id="ccl-search" class="ccl-b-input" name="queryString" placeholder="Start typing to search"/>
 
-        <div class="ccl-c-search">
+        <div class="ccl-c-search-bar__option">
+            <strong class="ccl-u-faded">As:</strong>
+            <select class="ccl-b-select ccl-c-search-index" name="index" title="Index">
+                <option value="ti">Title</option>
+                <option value="kw" selected="selected">Keyword</option>
+                <option value="au">Author</option>
+                <option value="su">Subject</option>
+            </select>
+        </div>
+        
+        <div class="ccl-c-search-bar__option">
+            <strong class="ccl-u-faded">In:</strong>
+            <select class="ccl-b-select" name="database" title="Database">
+                <option value="all" selected="selected">All</option>
+                <option value="Z-wcorg">WorldCat</option>
+                <option value="Z-oais">OAlster</option>
+                <option value="Bcnf">PapersFirst</option>
+            </select>
+        </div>
+    
+        <!-- <button type="submit" class="ccl-c-search-bar__submit ccl-b-btn ccl-is-solid" style="min-width: 8rem">
+            <i class="ccl-b-icon-search" aria-hidden="true"></i>
+            <span class="ccl-u-display-none">Search</span>
+        </button> -->
+    </form>
 
-            <form class="ccl-c-search-bar" name="catalogSearch" action="http://ccl.on.worldcat.org/search" target="_blank">
-                <input type="text" id="ccl-search" class="ccl-b-input" name="queryString" placeholder="Start typing to search"/>
-
-                <div class="ccl-c-search-bar__option">
-                    <strong class="ccl-u-faded">As:</strong>
-                    <select class="ccl-b-select ccl-c-search-index" name="index" title="Index">
-                        <option value="ti">Title</option>
-                        <option value="kw" selected="selected">Keyword</option>
-                        <option value="au">Author</option>
-                        <option value="su">Subject</option>
-                    </select>
-                </div>
-                
-                <div class="ccl-c-search-bar__option">
-                    <strong class="ccl-u-faded">In:</strong>
-                    <select class="ccl-b-select" name="database" title="Database">
-                        <option value="all" selected="selected">All</option>
-                        <option value="Z-wcorg">WorldCat</option>
-                        <option value="Z-oais">OAlster</option>
-                        <option value="Bcnf">PapersFirst</option>
-                    </select>
-                </div>
-            
-                <button type="submit" class="ccl-c-search-bar__submit ccl-b-btn ccl-is-solid" style="min-width: 8rem">
-                    <i class="ccl-b-icon-search" aria-hidden="true"></i>
-                    <span class="ccl-u-display-none">Search</span>
-                </button>
-            </form>
-
-            <div class="ccl-c-search-results">
-                <ul class="ccl-c-search-results__list">
-
-                </ul>
-            </div>
+    <div class="ccl-c-search-results">
+        <div class="ccl-c-search-results__list" role="list">
 
         </div>
-
     </div>
 
 </div>
