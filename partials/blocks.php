9a32bd8ee0a6ed42b92c4169c3bec9908b477929
Kirk Pettinga <kirk.pettinga@gmail.com>
2017-12-18 15:58:38 +0100

    add event block type with dummy content #63

--- partials/blocks.php
+++ partials/blocks.php
@@ -67,7 +67,7 @@ if ( $blocks ) : ?>
                                     $image_size = ( $enable_carousel ) ? 'thumbnail' : 'medium';
                                     $image_link = get_post_meta( $image_id, 'attachment_link', true ); ?>
 
-                                    <article class="ccl-c-carousel__slide" data-link="<?php echo $image_link; ?>">
+                                    <article class="ccl-c-carousel__slide">
                                         <?php if ( $image_link ) : ?>
                                             <a href="<?php echo $image_link; ?>">
                                         <?php endif; ?>
@@ -77,6 +77,97 @@ if ( $blocks ) : ?>
                                         <?php if ( $image_link ) : ?>
                                             </a>
                                         <?php endif; ?>
+                                    </article>
+
+                                <?php endforeach; ?>
+                                
+                            </div>
+
+                        </div>
+
+                    <?php endif; ?>
+
+                </div>
+
+            </div>
+
+        <?php elseif ( 'events' == $block['block_type'] ) : ?>
+
+            <?php 
+            $events = array();
+            $count = 6;
+            do {
+                array_push( $events, array(
+                    'title' => 'Event Title',
+                    'start' => 'Day, Month 00, 7:00',
+                    'end' => '8:00p',
+                    'venue' => 'Venue Name',
+                    'image' => 'http://placehold.it/500x350',
+                    'url' => '#'
+                ) );
+                $count--;
+            } while ( $count > 0 );
+            
+            $has_block_items = ( isset ( $events ) && $events );
+            $block_item_count = ( $has_block_items && is_array( $events ) ) ? count( $events ) : 0;
+            $enable_carousel = $block_item_count > 3; ?>
+
+            <div id="block-<?php echo $index; ?>" class="ccl-l-container">
+
+                <div class="ccl-c-promo">
+
+                    <header class="ccl-c-promo__header">
+
+                        <?php if ( isset( $block['block_title'] ) && $block['block_title'] ) : ?>
+
+                            <div class="ccl-c-promo__title"><?php echo $block['block_title']; ?></div>
+
+                        <?php endif; ?>
+
+                        <?php if ( isset( $block['block_cta'] ) && $block['block_cta'] ) : ?>
+
+                            <div class="ccl-c-promo__cta">
+                                <?php echo apply_filters( 'the_content', $block['block_cta'] ); ?>
+                            </div>
+
+                        <?php endif; ?>
+                        
+                        <?php if ( $enable_carousel ) : ?>
+
+                            <div class="ccl-c-promo__action">
+                                <button id="carousel-<?php echo $index; ?>-prev" class="ccl-b-btn--circular prev" aria-label="previous slide">&larr;</button>
+                                <button id="carousel-<?php echo $index; ?>-next" class="ccl-b-btn--circular next" aria-label="next slide">&rarr;</button>
+                            </div>
+
+                        <?php endif; ?>
+
+                    </header>
+
+                    <?php if ( $has_block_items ) : ?>
+
+                        <?php $carousel_class = ( $enable_carousel ) ? 'js-promo-carousel' : 'ccl-is-static'; ?>
+
+                        <div class="ccl-c-promo__content">
+
+                            <div class="ccl-c-carousel <?php echo $carousel_class; ?>" data-slick='{ "slidesToShow": 2, "prevArrow": "#carousel-<?php echo $index; ?>-prev", "nextArrow": "#carousel-<?php echo $index; ?>-next" }'>
+
+                                <?php if ( isset( $block['block_description'] ) && $block['block_description'] ) : ?>
+
+                                    <article class="ccl-c-promo__description ccl-c-carousel__slide">
+                                        <div style="max-width:300px"><?php echo apply_filters( 'the_content', $block['block_description'] ); ?></div>
+                                    </article>
+
+                                <?php endif; ?>
+
+                                <?php foreach ( (array) $events as $i => $event ) : ?>
+
+                                    <article class="ccl-c-carousel__slide">
+                                        <a href="<?php echo $event['url']; ?>">
+                                            <div class="ccl-u-mb-nudge"><img src="<?php echo $event['image'] ?>" alt="Event image" /></div>
+                                            <p class="ccl-h4 ccl-u-mt-0"><?php echo $event['title']; ?></p>
+                                            <p class="ccl-h4 ccl-u-mt-0 ccl-u-faded"><?php echo $event['start'] . '-' . $event['end']; ?></p>
+                                            <p class="ccl-h4 ccl-u-mt-0 ccl-u-faded"><?php echo $event['venue']; ?></p>
+                                        </a>
                                     </article>
 
                                 <?php endforeach; ?>
