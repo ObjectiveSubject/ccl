97bde8fc636dccab0b741a2c20807820c1bff07b
Kirk Pettinga <kirk.pettinga@gmail.com>
2017-12-19 12:10:46 +0100

    add school footer band #65

--- footer.php
+++ footer.php
@@ -6,6 +6,35 @@
 $footer_options = get_option( 'footer-options' );
 ?>
 
+	<div class="ccl-c-school-band ccl-u-py-2" style="background-color:#F5F5F5">
+		<div class="ccl-l-container">
+			<ul class="ccl-l-row ccl-l-align-center ccl-u-clean-list">
+				<?php 
+				$schools = array(
+					array( 'name' => 'CGU', 				'url' => 'https://www.cgu.edu/', 			 'logo' => CCL_ASSETS . 'images/logos/cgu@2x.png', 				'width' => 108, 'height' => 58 ),
+					array( 'name' => 'CUC', 				'url' => 'http://www.cuc.claremont.edu/', 	 'logo' => CCL_ASSETS . 'images/logos/cuc@2x.png', 				'width' => 72, 'height' => 78 ),
+					array( 'name' => 'Claremont McKenna', 	'url' => 'http://www.claremontmckenna.edu/', 'logo' => CCL_ASSETS . 'images/logos/claremontmckenna@2x.png', 'width' => 84, 'height' => 70 ),
+					array( 'name' => 'Harvey Mudd', 		'url' => 'https://www.hmc.edu/', 			 'logo' => CCL_ASSETS . 'images/logos/hmc@2x.png', 				'width' => 64, 'height' => 66 ),
+					array( 'name' => 'KGI', 				'url' => 'http://www.kgi.edu/', 			 'logo' => CCL_ASSETS . 'images/logos/kgi@2x.png', 				'width' => 158, 'height' => 36 ),
+					array( 'name' => 'Pitzer', 				'url' => 'https://www.pitzer.edu/', 		 'logo' => CCL_ASSETS . 'images/logos/pitzer@2x.png', 			'width' => 114, 'height' => 36 ),
+					array( 'name' => 'Pomona', 				'url' => 'https://www.pomona.edu/', 		 'logo' => CCL_ASSETS . 'images/logos/pomona@2x.png', 			'width' => 96, 'height' => 40 ),
+					array( 'name' => 'Scripps', 			'url' => 'http://www.scrippscollege.edu/', 	 'logo' => CCL_ASSETS . 'images/logos/scrippscollege@2x.png',	'width' => 86, 'height' => 74 ),
+				);
+				foreach ( $schools as $school ) : ?>
+					<li class="ccl-l-column">
+						<a href="<?php echo $school['url']; ?>" title="<?php echo $school['name']; ?>">
+							<img src="<?php echo $school['logo']; ?>" 
+								 class="ccl-u-display-block ccl-l-center" 
+								 width="<?php echo $school['width']; ?>" 
+								 height="<?php echo $school['height']; ?>" 
+								 alt="<?php echo $school['name'] . ' logo'; ?>" />
+						</a>
+					</li>
+				<?php endforeach; ?>
+			</ul>
+		</div>
+	</div>
+
 	<footer id="colophon" class="ccl-c-footer ccl-u-pt-2 ccl-u-pb-3" role="contentinfo">
 		
 		<div class="ccl-l-container">
