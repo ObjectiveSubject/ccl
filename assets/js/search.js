f2cd565a88fc34a987a650fdc005626c566380b7
Gabriel Ortiz <gabriel_ortiz@cuc.claremont.edu>
2018-01-20 21:59:40 +0000

    added scope searching to search-box and submit on enter #70

--- assets/js/search.js
+++ assets/js/search.js
@@ -21,15 +21,16 @@
 		
      var SearchAutocomplete = function(elem){
 		
-		this.$el = $(elem);
-		this.$form = this.$el.find('form');
-		this.$input = $(elem).find('#ccl-search');
-		this.$response = this.$el.find('.ccl-c-search-results');
-		this.$responseList = this.$el.find('.ccl-c-search-results__list');
+		this.$el			= $(elem);
+		this.$form			= this.$el.find('form');
+		this.$input 		= $(elem).find('#ccl-search');
+		this.$response		= this.$el.find('.ccl-c-search-results');
+		this.$responseList	= this.$el.find('.ccl-c-search-results__list');
 		this.$responseItems = this.$el.find('.ccl-c-search-item');
-		this.$resultsLink = this.$el.find('.ccl-c-search-results__footer');
-		this.$searchIndex = this.$el.find('.ccl-c-search-index');
-		this.$worldCatLink = null;
+		this.$resultsLink	= this.$el.find('.ccl-c-search-results__footer');
+		this.$searchIndex	= this.$el.find('.ccl-c-search-index');
+		this.$searchScope = this.$el.find('.ccl-c-search-location');
+		this.$worldCatLink	= null;
 
         this.init();
      };
@@ -73,7 +74,7 @@
 				
 			})
 			.blur(function(event){
-				console.log('input blurred');
+				console.log(' input blurred');
 				$(document).on('click', _onBlurredClick);
 
 				function _onBlurredClick(event) {
@@ -89,22 +90,35 @@
 				}
 			});
 
-		this.$searchIndex.change(function(){
+		this.$searchIndex.add(this.$searchScope).change(function(){
 			_this.onSearchIndexChange();
 		});
 
+		//on submit fire off catalog search to WMS
 		this.$form.submit(function(event) {
 
 			event.preventDefault();
-
-			var query = _this.$input.val();
-
 			clearTimeout(timeout);
+			
+			//get search index and input value
+			var searchIndex = _this.$searchIndex.val();
+			var queryString = _this.$input.val();
+			
+			//setup array for constructSearchURL()
+			var input_array = [];
+			input_array['queryString']		= searchIndex + ":" + queryString;
+			input_array['searchScope']	= _this.$searchScope.val();
 
-			if ( query.length > 1 ) {
+			//if query string has content, then run
+			if ( queryString.length > 1 ) {
 				_this.$response.show();
-				_this.fetchResults( query );
+				_this.fetchResults( queryString );
+
+				var wmsConstructedUrl = _this.constructSearchURL(input_array);
+				
+				window.open(wmsConstructedUrl, '_blank');
 		   }
+		   
 		});
 	};
 
@@ -134,29 +148,39 @@
 
 	SearchAutocomplete.prototype.fetchResultsDONE = function( response ) {
 		
+		//console.log( response );
+		
 		var _this = this,
 			results = $.parseJSON(response),
 			count = results.count,
 			query = results.query,
 			posts = results.posts,
 			searchIndex = _this.$searchIndex.val(),
-			searchIndexNicename = indexNames[searchIndex];
-
+			searchIndexNicename = indexNames[searchIndex],
+			searchScopeData = $( _this.$searchScope );
+			
 		// wrap query
-		var queryString = searchIndex + ':(' + query + ')';
+		var queryString = searchIndex + ':' + query;
+		
+		//get wms_url input_array = [queryString, searchScope]
+		var input_array = [];
+		input_array['queryString']	= queryString;
+		input_array['searchScope']	= searchScopeData.val();
+		
+		var wmsConstructedUrl = _this.constructSearchURL(input_array);
 
 		// Clear response area list items (update when Pattern Library view isn't necessary)
 		_this.$responseList.html('');
 		_this.$resultsLink.remove();
 
 		// Create list item for Worldcat search.
-		var listItem =  '<a href="https://ccl.on.worldcat.org/external-search?sortKey=library&queryString=' + queryString + '" class="ccl-c-search-item ccl-is-large" role="listitem" target="_blank">' +
+		var listItem =  '<a href="'+ wmsConstructedUrl +'" class="ccl-c-search-item ccl-is-large" role="listitem" target="_blank">' +
 							'<span class="ccl-c-search-item__type">' +
 								'<i class="ccl-b-icon book" aria-hidden="true"></i>' +
 								'<span class="ccl-c-search-item__type-text">WorldCat</span>' +
 							'</span>' +
 							'<span class="ccl-c-search-item__title\">' +
-								'Search by ' + searchIndexNicename + ' for &ldquo;' + query + '&rdquo; on WorldCat ' +
+								'Search by ' + searchIndexNicename + ' for &ldquo;' + query + '&rdquo; in '+ searchScopeData.find('option:selected').text() +' ' +
 								'<i class="ccl-b-icon arrow-right" aria-hidden="true" style="vertical-align:middle"></i>' +
 							'</span>' +
 							'<span class="ccl-c-search-item__cta">' +
@@ -241,6 +265,52 @@
 
 		this.fetchResults( query );
 	};
+	
+	SearchAutocomplete.prototype.constructSearchURL = function(input_array){
+		//constructs URL with parameters from
+		//input_array = [queryString, searchScope]
+		
+		//define variables
+		var queryString, searchSrc, searchScopeKey, wms_url;
+		
+		queryString = input_array['queryString'];
+		searchSrc	= input_array['searchScope'];
+		
+		//check if search location is a scoped search location
+		if( searchSrc.match(/::zs:/) ){
+			searchScopeKey = 'subscope';
+		}else{
+			searchScopeKey = 'scope';
+		}
+		
+            //build the URL
+            var wms_params = {
+                sortKey         : 'LIBRARY',
+                databaseList    : '',
+                queryString     : queryString,
+                Facet           : '',
+                //scope added below
+                //format added below
+                format			: 'all',
+                database        :  'all',
+                author          : '',
+                year            : 'all',
+                yearFrom        : '',
+                yearTo          : '',
+                language        : 'all',
+                topic           : ''
+            };
+            
+            wms_params[searchScopeKey] = searchSrc;
+            
+            wms_url = 'https://ccl.on.worldcat.org/search?' + $.param(wms_params);
+            wms_url = wms_url.replace( '%26', "&" );
+            
+            //console.log(wms_url);
+            
+            return wms_url;
+
+    };
 
 	// If the user hits return or presses submit, the query will go directly to WorldCat. 
 	// This function wraps the string in the specified index value (keyword, author, title, etc)
--- components/search-box.php
+++ components/search-box.php
@@ -2,11 +2,11 @@
 
     <form class="ccl-c-search-form" name="catalogSearch" action="http://ccl.on.worldcat.org/search" target="_blank">
         <label for="ccl-search" class="ccl-u-display-none">Start typing to search</label>
-        <input type="text" id="ccl-search" class="ccl-b-input" name="queryString" placeholder="Start typing to search"/>
+        <input type="text" id="ccl-search" class="ccl-b-input" name="queryString" placeholder="Start typing to search" aria-label="Search"/>
 
         <div class="ccl-c-search-form__option">
             <strong>As:</strong>
-            <select class="ccl-b-select ccl-c-search-index" name="index" title="Index">
+            <select class="ccl-b-select ccl-c-search-index" name="index" title="Index" aria-label="Search Index">
                 <option value="ti">Title</option>
                 <option value="kw" selected="selected">Keyword</option>
                 <option value="au">Author</option>
@@ -16,11 +16,10 @@
         
         <div class="ccl-c-search-form__option">
             <strong>In:</strong>
-            <select class="ccl-b-select" name="database" title="Database">
-                <option value="all" selected="selected">All Databases</option>
-                <option value="Z-wcorg">WorldCat</option>
-                <option value="Z-oais">OAlster</option>
-                <option value="Bcnf">PapersFirst</option>
+            <select class="ccl-b-select ccl-c-search-location" name="location" title="Location" aria-label="Search Location">
+                <option value="" selected="selected">Libraries Worldwide</option>
+                <option value="wz:519">Claremont Colleges Library</option>
+                <option value="wz:519::zs:36307">Special Collections</option>
             </select>
         </div>
     
