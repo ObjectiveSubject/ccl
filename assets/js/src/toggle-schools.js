cee5f6b250c53ff52642a2118ca4aa9754108922
Dave Kellam <dave@eightface.com>
2018-01-28 02:46:06 -0500

    Store selected school in cookie
    #68

--- assets/js/src/toggle-schools.js
+++ assets/js/src/toggle-schools.js
@@ -18,19 +18,55 @@
     };
 
     SchoolSelect.prototype.init = function(){
-        
+
+        var school = getCookie( 'ccl-school' );
+
         if ( initSchool ) {
 
             this.$select
-                .find( 'option[value="' + initSchool + '"]' )
-                .attr( 'selected', 'selected' );   
+                .find( 'option[value="' + school + '"]' )
+                .attr( 'selected', 'selected' );
 
-        }
+        	if ( school ) {
+        		 $('html').attr('data-school', school);
+			}
+
+		}
 
         this.$select.change(function(event){
-            $('html').attr(  'data-school', event.target.value );
+            $('html').attr( 'data-school', event.target.value );
+
+            eraseCookie( 'ccl-school' );
+            setCookie( 'ccl-school', event.target.value, 7);
         });
     };
+
+    // Cookie functions lifted from Stack Overflow for now
+    // https://stackoverflow.com/questions/14573223/set-cookie-and-get-cookie-with-javascript
+	function setCookie(name, value, days) {
+		var expires = "";
+		if (days) {
+			var date = new Date();
+			date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+			expires = "; expires=" + date.toUTCString();
+		}
+		document.cookie = name + "=" + (value || "") + expires + "; path=/";
+	}
+
+	function getCookie(name) {
+		var nameEQ = name + "=";
+		var ca = document.cookie.split(';');
+		for (var i = 0; i < ca.length; i++) {
+			var c = ca[i];
+			while (c.charAt(0) === ' ') c = c.substring(1, c.length);
+			if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
+		}
+		return null;
+	}
+
+	function eraseCookie(name) {
+		document.cookie = name + '=; Max-Age=-99999999;';
+	}
 
     $(document).ready(function(){
         $('[data-toggle="school"]').each(function(){
