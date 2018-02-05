4931b5fc0b7300e273ee5abe166143bf21a28e5a
Gabriel Ortiz <gabriel_ortiz@cuc.claremont.edu>
2018-02-05 23:59:55 +0000

    allow users to add more rooms through wp

--- includes/rooms.php
+++ includes/rooms.php
@@ -49,7 +49,7 @@ function register_rooms_post_type() {
 		'supports'        => array( 'title' ), // content, editor, thumbnail would allow content to be edited
 		'capability_type' => 'post',
 		'capabilities' => array(
-			'create_posts' => 'do_not_allow', // Remove support for "Add New" (can also change to a role, rather than false)
+			//'create_posts' => 'do_not_allow', // Remove support for "Add New" (can also change to a role, rather than false)
 		),
 		'map_meta_cap' => true, // Allows created posts to be edited
 	) );
